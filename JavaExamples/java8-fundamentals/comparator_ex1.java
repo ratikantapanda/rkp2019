537924e2e936efeeed78bd7bb9328c372074a1df
ratikantapanda <panda.ratikanta@gmail.com>
2019-10-23 22:16:07 -0400

    Add files via upload

---
+++ JavaExamples/java8-fundamentals/comparator_ex1.java
@@ -0,0 +1,68 @@
+// Java program to demonstrate working of Comparator 
+// interface 
+import java.util.*; 
+import java.lang.*; 
+import java.io.*; 
+
+// A class to represent a student. 
+class Student 
+{ 
+	int rollno; 
+	String name, address; 
+
+	public Student(int rollno, String name, String address) 
+	{ 
+		this.rollno = rollno; 
+		this.name = name; 
+		this.address = address; 
+	} 
+	public String toString() 
+	{ 
+		return this.rollno + " " + this.name + " " + this.address; 
+	} 
+} 
+
+class Sortbyroll implements Comparator<Student> 
+{ 
+	public int compare(Student a, Student b) 
+	{ 
+		return a.rollno - b.rollno; 
+	} 
+} 
+
+class Sortbyname implements Comparator<Student> 
+{ 
+	public int compare(Student a, Student b) 
+	{ 
+		return a.name.compareTo(b.name); 
+	} 
+} 
+
+// Driver class 
+class ComparatorTest 
+{ 
+	public static void main (String[] args) 
+	{ 
+		ArrayList<Student> ar = new ArrayList<Student>(); 
+		ar.add(new Student(111, "bbbb", "london")); 
+		ar.add(new Student(131, "aaaa", "nyc")); 
+		ar.add(new Student(121, "cccc", "jaipur")); 
+
+		System.out.println("Unsorted"); 
+		for (int i=0; i<ar.size(); i++) 
+			System.out.println(ar.get(i)); 
+
+		Collections.sort(ar, new Sortbyroll()); 
+
+		System.out.println("\nSorted by rollno"); 
+		for(Student s: ar){
+			System.out.println(s);
+		}
+
+		Collections.sort(ar, new Sortbyname()); 
+		System.out.println("\nSorted by name"); 
+		//ar.stream().forEach(s->System.out.println(s));
+		ar.stream().forEach(System.out::println);
+	} 
+} 
+
